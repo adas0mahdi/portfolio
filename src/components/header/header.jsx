a859c8203ff5fb67a248963396dd56c253840fd1
adas0mahdi <adas0mahdi@gmail.com>
2023-06-27 12:02:40 +0800

    .

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,15 @@
 import "./header.css";
 
 function Header() {
-    return (
-        <div id="home" className="container header-container">
-            <h3>
-                <p>
-                    I<span>'</span>m
-                </p>
-                Adas 
-                <h2>Freelancer</h2>
-            </h3>
-        </div>
-    );
+  return (
+    <div id="home" className="container header-container">
+      <h3>
+        <p>I<span>'</span>m</p>
+        Adas
+      </h3>
+      <h2 className="header-subtitle">Freelancer</h2>
+    </div>
+  );
 }
 
 export default Header;
