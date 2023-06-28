b9d3ebeade6d1ce08a2e9b60a1487d1c192ba923
adas0mahdi <adas0mahdi@gmail.com>
2023-06-28 21:19:41 +0800

    .

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -4,7 +4,6 @@ const Projects = () => {
   return (
     <div id="projects" className="container projects-container">
       <h1 className="project-txt">PROJRCTS</h1>
-
       <div className="project ">
         <div className="project-img img-a"></div>
         <div className="project-info">
@@ -13,7 +12,20 @@ const Projects = () => {
           <h4 className="about">
           CineZone: Discover, Connect, and Experience Movies like Never Before! Unleash your passion for films, explore genres, watch trailers, and engage with a community of movie enthusiasts. Join CineZone today and embark on an unforgettable cinematic journey!
           </h4>
-          <a href="#contact" className="contact-project">
+          <a href="https://github.com/adas0mahdi/CineZone.git" className="contact-project">
+            <span>Project</span>
+          </a>
+        </div>
+      </div>
+      <div className="project ">
+        <div className="project-img img-g"></div>
+        <div className="project-info">
+          <h1 className="name">feedback</h1>
+          <h3 className="languageUsed">PHP</h3>
+          <h4 className="about">
+          Enhance your feedback management with feedback! Collect, analyze, and utilize valuable insights to improve your products or services. Streamline the feedback process and empower your decision-making. With feedback, listen to your audience and elevate customer satisfaction effortlessly.
+          </h4>
+          <a href="https://github.com/adas0mahdi/feedback.php.git" className="contact-project">
             <span>Project</span>
           </a>
         </div>
@@ -21,12 +33,12 @@ const Projects = () => {
       <div className="project ">
         <div className="project-img img-b"></div>
         <div className="project-info">
-          <h1 className="name">Project Name</h1>
-          <h3 className="languageUsed">HTML, CSS , JS</h3>
+          <h1 className="name">HotelResponsive</h1>
+          <h3 className="languageUsed">HTML, CSS</h3>
           <h4 className="about">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quae omnis vitae, iusto mollitia eligendi nesciunt non consequuntur aliquid! Sint?
+          Unleash luxury with HotelResponsive! Discover captivating design, seamless navigation, and exclusive accommodations. Book your dream stay, indulge in exceptional service, and create unforgettable memories at every moment. Elevate your travel experience today!
           </h4>
-          <a href="#contact" className="contact-project">
+          <a href="https://github.com/adas0mahdi/03_HotelResponsive.git" className="contact-project">
             <span>Project</span>
           </a>
         </div>
@@ -34,12 +46,12 @@ const Projects = () => {
       <div className="project ">
         <div className="project-img img-c"></div>
         <div className="project-info">
-          <h1 className="name">Project Name</h1>
-          <h3 className="languageUsed">HTML, CSS , JS</h3>
+          <h1 className="name">KodeResto</h1>
+          <h3 className="languageUsed">HTML, CSS</h3>
           <h4 className="about">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quae omnis vitae, iusto mollitia eligendi nesciunt non consequuntur aliquid! Sint?
+          Embark on a gastronomic journey at KodeResto! Explore a tantalizing menu, savor delightful flavors, and immerse yourself in a captivating dining atmosphere. With impeccable service and a commitment to culinary excellence, KodeResto promises a dining experience that will leave a lasting impression. Discover the perfect blend of taste, ambiance, and hospitality at KodeResto.
           </h4>
-          <a href="#contact" className="contact-project">
+          <a href="https://github.com/adas0mahdi/08_KodeResto.git" className="contact-project">
             <span>Project</span>
           </a>
         </div>
@@ -47,12 +59,12 @@ const Projects = () => {
       <div className="project ">
         <div className="project-img img-d"></div>
         <div className="project-info">
-          <h1 className="name">Project Name</h1>
+          <h1 className="name">image search engine</h1>
           <h3 className="languageUsed">HTML, CSS , JS</h3>
           <h4 className="about">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem,
+          Discover and Unleash the Power of Visual Exploration with the Image Search Engine. Find the perfect images based on your preferences using advanced algorithms. Effortlessly search, refine, and explore a vast collection of visually similar images. Fuel your creativity, research, and inspiration with the Image Search Engine today!
           </h4>
-          <a href="#contact" className="contact-project">
+          <a href="https://github.com/adas0mahdi/image-search-engine.git" className="contact-project">
             <span>Project</span>
           </a>
         </div>
@@ -60,12 +72,12 @@ const Projects = () => {
       <div className="project ">
         <div className="project-img img-e"></div>
         <div className="project-info">
-          <h1 className="name">Project Name</h1>
+          <h1 className="name">KodeShop</h1>
           <h3 className="languageUsed">HTML, CSS , JS</h3>
           <h4 className="about">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem,
+          Experience seamless online shopping with KodeShop! Discover a world of products, from electronics to fashion, all in one place. Enjoy secure transactions, efficient order management, and exceptional customer support. Elevate your shopping experience with KodeShop today!
           </h4>
-          <a href="#contact" className="contact-project">
+          <a href="https://github.com/adas0mahdi/KodaShop-2023.git" className="contact-project">
             <span>Project</span>
           </a>
         </div>
@@ -73,12 +85,25 @@ const Projects = () => {
       <div className="project ">
         <div className="project-img img-f"></div>
         <div className="project-info">
-          <h1 className="name">Project Name</h1>
-          <h3 className="languageUsed">HTML, CSS , JS</h3>
+          <h1 className="name">Kodeledger</h1>
+          <h3 className="languageUsed">HTML, CSS</h3>
           <h4 className="about">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem,
+          Master your finances with Kodeledger! Simplify bookkeeping, track transactions, and gain financial clarity effortlessly. Stay organized and in control with this intuitive ledger management system. Experience seamless financial management with Kodeledger and take charge of your financial success.
           </h4>
-          <a href="#contact" className="contact-project">
+          <a href="https://github.com/adas0mahdi/04_Kodeledger.git" className="contact-project">
+            <span>Project</span>
+          </a>
+        </div>
+      </div>
+      <div className="project ">
+        <div className="project-img img-h"></div>
+        <div className="project-info">
+          <h1 className="name">NewsGrid</h1>
+          <h3 className="languageUsed">HTML, CSS</h3>
+          <h4 className="about">
+          NewsGrid, the ultimate news website template. Stay up-to-date with the latest news in a visually stunning and user-friendly environment. Explore diverse categories, dive into captivating articles, and enjoy a seamless reading experience across all devices. NewsGrid - your go-to destination for news that matters.
+          </h4>
+          <a href="https://github.com/adas0mahdi/NewsGrid.git" className="contact-project">
             <span>Project</span>
           </a>
         </div>
