8dd1f671d2a83c3b4b7cb524c3ccac383b701950
adas0mahdi <adas0mahdi@gmail.com>
2023-06-28 18:09:17 +0800

    .

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -5,34 +5,21 @@ const Projects = () => {
     <div id="projects" className="container projects-container">
       <h1 className="project-txt">PROJRCTS</h1>
 
-      <div className="project project-3">
-        <div className="project-img"></div>
+      <div className="project ">
+        <div className="project-img img-a"></div>
         <div className="project-info">
           <h1 className="name">Project Name</h1>
           <h3 className="languageUsed">HTML, CSS , JS</h3>
           <h4 className="about">
-           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quae omnis vitae, iusto mollitia eligendi nesciunt non consequuntur aliquid! Sint?,
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quae omnis vitae, iusto mollitia eligendi nesciunt non consequuntur aliquid! Sint?,
           </h4>
           <a href="#contact" className="contact-project">
             <span>Project</span>
           </a>
         </div>
       </div>
-      <div className="project project-3">
-        <div className="project-img"></div>
-        <div className="project-info">
-          <h1 className="name">Project Name</h1>
-          <h3 className="languageUsed">HTML, CSS , JS</h3>
-          <h4 className="about">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quae omnis vitae, iusto mollitia eligendi nesciunt non consequuntur aliquid! Sint?
-          </h4>
-          <a href="#contact" className="contact-project">
-            <span>Project</span>
-          </a>
-        </div>
-      </div>
-      <div className="project project-3">
-        <div className="project-img"></div>
+      <div className="project ">
+        <div className="project-img img-b"></div>
         <div className="project-info">
           <h1 className="name">Project Name</h1>
           <h3 className="languageUsed">HTML, CSS , JS</h3>
@@ -44,8 +31,47 @@ const Projects = () => {
           </a>
         </div>
       </div>
-      <div className="project project-3">
-        <div className="project-img"></div>
+      <div className="project ">
+        <div className="project-img img-c"></div>
+        <div className="project-info">
+          <h1 className="name">Project Name</h1>
+          <h3 className="languageUsed">HTML, CSS , JS</h3>
+          <h4 className="about">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quae omnis vitae, iusto mollitia eligendi nesciunt non consequuntur aliquid! Sint?
+          </h4>
+          <a href="#contact" className="contact-project">
+            <span>Project</span>
+          </a>
+        </div>
+      </div>
+      <div className="project ">
+        <div className="project-img img-d"></div>
+        <div className="project-info">
+          <h1 className="name">Project Name</h1>
+          <h3 className="languageUsed">HTML, CSS , JS</h3>
+          <h4 className="about">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem,
+          </h4>
+          <a href="#contact" className="contact-project">
+            <span>Project</span>
+          </a>
+        </div>
+      </div>
+      <div className="project ">
+        <div className="project-img img-e"></div>
+        <div className="project-info">
+          <h1 className="name">Project Name</h1>
+          <h3 className="languageUsed">HTML, CSS , JS</h3>
+          <h4 className="about">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem,
+          </h4>
+          <a href="#contact" className="contact-project">
+            <span>Project</span>
+          </a>
+        </div>
+      </div>
+      <div className="project ">
+        <div className="project-img img-f"></div>
         <div className="project-info">
           <h1 className="name">Project Name</h1>
           <h3 className="languageUsed">HTML, CSS , JS</h3>
