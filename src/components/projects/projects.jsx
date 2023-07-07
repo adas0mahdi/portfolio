16239f35269820e77e1c2996bd6406e61211dda3
adas0mahdi <adas0mahdi@gmail.com>
2023-07-07 11:16:58 +0800

    .

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -5,10 +5,23 @@ const Projects = () => {
     <div id="projects" className="container projects-container">
       <h1 className="project-txt">PROJRCTS</h1>
       <div className="project ">
+        <div className="project-img img-i"></div>
+        <div className="project-info">
+          <h1 className="name">PlayKoDEX</h1>
+          <h3 className="languageUsed">HTML, CSS, JS</h3>
+          <h4 className="about">
+          PlayKoDEX is a Game Database and Gaming News website developed as a project for the KodeGo Bootcamp. The website allows users to explore a wide range of games, read the latest gaming news, and stay up-to-date with the gaming industry.
+          </h4>
+          <a href="https://github.com/adas0mahdi/playkodex.git" className="contact-project">
+            <span>Project</span>
+          </a>
+        </div>
+      </div>
+      <div className="project ">
         <div className="project-img img-a"></div>
         <div className="project-info">
           <h1 className="name">CineZone</h1>
-          <h3 className="languageUsed">HTML, CSS , JS</h3>
+          <h3 className="languageUsed">HTML, CSS, JS</h3>
           <h4 className="about">
           CineZone: Discover, Connect, and Experience Movies like Never Before! Unleash your passion for films, explore genres, watch trailers, and engage with a community of movie enthusiasts. Join CineZone today and embark on an unforgettable cinematic journey!
           </h4>
@@ -60,7 +73,7 @@ const Projects = () => {
         <div className="project-img img-d"></div>
         <div className="project-info">
           <h1 className="name">image search engine</h1>
-          <h3 className="languageUsed">HTML, CSS , JS</h3>
+          <h3 className="languageUsed">HTML, CSS, JS</h3>
           <h4 className="about">
           Discover and Unleash the Power of Visual Exploration with the Image Search Engine. Find the perfect images based on your preferences using advanced algorithms. Effortlessly search, refine, and explore a vast collection of visually similar images. Fuel your creativity, research, and inspiration with the Image Search Engine today!
           </h4>
@@ -73,7 +86,7 @@ const Projects = () => {
         <div className="project-img img-e"></div>
         <div className="project-info">
           <h1 className="name">KodeShop</h1>
-          <h3 className="languageUsed">HTML, CSS , JS</h3>
+          <h3 className="languageUsed">HTML, CSS, JS</h3>
           <h4 className="about">
           Experience seamless online shopping with KodeShop! Discover a world of products, from electronics to fashion, all in one place. Enjoy secure transactions, efficient order management, and exceptional customer support. Elevate your shopping experience with KodeShop today!
           </h4>
